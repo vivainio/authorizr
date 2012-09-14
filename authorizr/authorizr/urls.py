7cf22d44effddd10fab28b1a0042a73ec7e691b8
Ville M. Vainio <vivainio@gmail.com>
2012-09-14 14:10:40 +0300

    added appreg project

--- authorizr/authorizr/urls.py
+++ authorizr/authorizr/urls.py
@@ -4,12 +4,17 @@ from django.conf.urls import patterns, include, url
 from django.contrib import admin
 admin.autodiscover()
 
+#from allauth.account.views import logout
+#from allauth.socialaccount.views import login_cancelled, login_error
+#from allauth.facebook.views import login as facebook_login
+
+
 urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'authorizr.views.home', name='home'),
     # url(r'^authorizr/', include('authorizr.foo.urls')),
 
-    (r'^accounts/', include('allauth.urls')),
+    url(r'^accounts/', include('allauth.urls')),
     # Uncomment the admin/doc line below to enable admin documentation:
     # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
 
