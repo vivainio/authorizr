1b2e2408789e88a886259d17cf2738de857295a9
Ville M. Vainio <vivainio@gmail.com>
2012-09-18 11:23:50 +0300

    add some models

--- authorizr/appreg/models.py
+++ authorizr/appreg/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+
+class AppOwner(models.Model):
+    uid = models.CharField(primary_key = True, max_length = 255)    
+    desc = models.CharField(max_length = 255)
+    
+
+class AppCredentials(models.Model):
+    app_desc = models.CharField(max_length = 255)
+    app_api_key = models.CharField(max_length = 255)
+    app_secret = models.CharField(max_length = 255)
+    owner = models.ForeignKey(AppOwner)
+    
+    
