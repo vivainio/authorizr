eeae8854452358cd4eb911f40ddffb5d630ba0bc
Ville M. Vainio <vivainio@gmail.com>
2012-09-19 16:24:11 +0300

    auth works

--- authorizr/appreg/views.py
+++ authorizr/appreg/views.py
@@ -4,8 +4,56 @@ from django.http import HttpResponse
 
 from django.shortcuts import render_to_response
 
+from models import AppCredentials
+
+from sanction.client import Client
+
+config = {
+          'google.client_id': "1037435290190.apps.googleusercontent.com",
+          'google.client_secret' : "1AG4Y2knGVYBvuPHi1sEFpJ8"
+          
+          }
+
 def index(request):
     
     return render_to_response('appreg/index.html', {'request': request})
 
+def myapps(request):    
+    return render_to_response('appreg/myapps.html', {'request': request})
+
+def mkclient():
+    c = Client(auth_endpoint="https://accounts.google.com/o/oauth2/auth",
+    token_endpoint="https://accounts.google.com/o/oauth2/token",
+    resource_endpoint="https://www.googleapis.com/oauth2/v1",
+    client_id=config["google.client_id"],
+    client_secret=config["google.client_secret"],
+    redirect_uri="http://localhost:8000/login/google")
+    return c
+
+
+def startlogin(request):
+    scope_req = ("https://www.googleapis.com/auth/userinfo.email",
+                 "https://www.googleapis.com/auth/userinfo.profile"
+                 )
+    
+    c = mkclient()
+    url = c.auth_uri(scope_req)
+    return render_to_response("appreg/startlogin.html", { "loginurl" : url})
+    
+        
+def googlelogin(request):
+
+    c = mkclient()
+    rdict = request.REQUEST
+    print "requesting token"
+    d = {'code' : rdict["code"]}
+    c.request_token(None, **d)
+    print "token received!"
+    print c
+    print "google login",rdict
+    
+    data = c.request("/userinfo")
+    print "Got data",data
+    
+    
     
--- authorizr/authorizr/urls.py
+++ authorizr/authorizr/urls.py
@@ -14,7 +14,10 @@ urlpatterns = patterns('',
     # url(r'^$', 'authorizr.views.home', name='home'),
     # url(r'^authorizr/', include('authorizr.foo.urls')),
     url("^appreg/$", "appreg.views.index"),
+    url("^appreg/myapps/", "appreg.views.myapps"),
     url(r'^accounts/', include('allauth.urls')),
+    url(r'^login/google/', 'appreg.views.googlelogin'),
+    url(r'^login/startlogin/', 'appreg.views.startlogin'),
     # Uncomment the admin/doc line below to enable admin documentation:
     # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
 
