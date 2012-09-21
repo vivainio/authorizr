1626385cfd2625cb29f36a616d3f62e909402375
salento <salento@salento-laptop.(none)>
2012-09-21 13:57:57 +0300

    Remove allauth

--- authorizr/authorizr/urls.py
+++ authorizr/authorizr/urls.py
@@ -15,7 +15,7 @@ urlpatterns = patterns('',
     # url(r'^authorizr/', include('authorizr.foo.urls')),
     url("^appreg/$", "appreg.views.index"),
     url("^appreg/myapps/", "appreg.views.myapps"),
-    url(r'^accounts/', include('allauth.urls')),
+    #url(r'^accounts/', include('allauth.urls')),
     url(r'^login/google/', 'appreg.views.googlelogin'),
     url(r'^login/startlogin/', 'appreg.views.startlogin'),
     # Uncomment the admin/doc line below to enable admin documentation:
