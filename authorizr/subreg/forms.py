ecb8d772f76ec0cd4c25dd391146a2bbcbb0d3c0
Juhana Koski <juhana@ubuntu.(none)>
2012-10-18 02:32:22 -0700

    Subreg changes

---
+++ authorizr/subreg/forms.py
@@ -0,0 +1,23 @@
+from django import forms
+from subapi.models import Resource
+from django.forms.widgets import TextInput
+
+class ResourceForm(forms.ModelForm):
+
+    def __init__(self, *args, **kwargs):
+        super(ResourceForm, self).__init__(*args, **kwargs)
+        
+        for field_name in self.fields:
+            field = self.fields.get(field_name)  
+            
+            if field:
+                if type(field.widget) is forms.TextInput:
+                    field.widget = forms.TextInput(attrs={
+                                                          'placeholder': field.label,                                                          
+                                                          'class':'input-xxlarge'})    
+    
+    class Meta:
+        model = Resource
+        exclude = ('uid','owner','redirect_uri')
+        
+    
