e358b6b3b807db171b3090cec79c035b07a21c45
Juhana Koski <juhana@ubuntu.(none)>
2012-10-22 02:57:58 -0700

    Subreg changes

--- authorizr/subapi/models.py
+++ authorizr/subapi/models.py
@@ -1,9 +1,17 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.core.exceptions import ValidationError
+import string
+import re
+
+def validate_res(value):
+    if re.match("^[a-zA-Z0-9]+(\.[a-zA-Z0-9]+)+$", value) is None:
+        raise ValidationError('Resource name must use following notation [ascii string].[ascii string]...')
+
 
 class Resource(models.Model):
     
-    resource_id = models.CharField(primary_key = True, max_length = 1024)
+    resource_id = models.CharField(primary_key = True, max_length = 1024,validators=[validate_res])
     owner = models.ForeignKey(User)
     
     description  = models.CharField(max_length = 1024)
