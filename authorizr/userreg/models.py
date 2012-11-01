120e037c4224dda737cf2871261bf9e56cb77bf1
isalento <isalento@gmail.com>
2012-11-01 11:28:11 +0200

    fixes

--- authorizr/userreg/models.py
+++ authorizr/userreg/models.py
@@ -5,6 +5,6 @@ from django.contrib.auth.models import User
 class OIConnectUser(User):
     user_id = models.CharField(primary_key = True, max_length = 255) 
     access_token = models.CharField(max_length=100)
-    expires = models.FloatField(default=-1)
+
 
 
