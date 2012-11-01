1653fea6285a1f94df0bd013c038a6debb32fcc7
Juhana Koski <juhana@ubuntu.(none)>
2012-11-01 06:25:56 -0700

    issue 29

--- authorizr/subapi/models.py
+++ authorizr/subapi/models.py
@@ -16,13 +16,13 @@ class Resource(models.Model):
     
     description  = models.CharField(max_length = 1024)
     sub_duration = models.PositiveIntegerField(null=True, blank=True)
-    sub_max_use_count = models.PositiveIntegerField(null=True, blank = True)
+    sub_max_credits_count = models.PositiveIntegerField(null=True, blank = True)
 
 class Subscription(models.Model):
     
     resource = models.ForeignKey(Resource)
     
     client_id = models.CharField(primary_key = True, max_length = 1024)
-    use_counter = models.PositiveIntegerField(null=True, blank = True)
+    credits_count = models.PositiveIntegerField(null=True, blank = True)
     expires = models.PositiveIntegerField(null=True, blank = True)   
     
