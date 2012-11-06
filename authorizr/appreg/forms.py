cddff6b695c0fbacc449bb26071758fc169685d2
isalento <isalento@gmail.com>
2012-11-06 14:23:00 +0200

    Toggle for OAuth1 / Oauth2

--- authorizr/appreg/forms.py
+++ authorizr/appreg/forms.py
@@ -11,17 +11,37 @@ from django.forms.widgets import TextInput
 
 class AppCredentialForm(forms.ModelForm):
 
+    oauth1_field_names = ["app_desc", "app_api_key", "app_secret", "auth_endpoint", "token_endpoint"]
+    oauth2_field_names = ["app_desc", "app_api_key", "app_secret", "scope","auth_endpoint", "token_endpoint", "resource_endpoint", "user_callback_page"]
+
+
     def __init__(self, *args, **kwargs):
         super(AppCredentialForm, self).__init__(*args, **kwargs)
         
+        def filterbyvalue(seq, value):
+            for el in seq:
+                if el.attribute==value: yield el
+                
+        
         for field_name in self.fields:
             field = self.fields.get(field_name)  
-            
+                        
             if field:
                 if type(field.widget) is forms.TextInput:
+                    
+                    classes  = "input-xxlarge "
+                                        
+                    if field_name in self.oauth1_field_names:
+                        classes += "oauth1 "
+                        
+                    if field_name in self.oauth2_field_names:
+                        classes += "oauth2 "
+                    
                     field.widget = forms.TextInput(attrs={
                                                           'placeholder': field.label,                                                          
-                                                          'class':'input-xxlarge'})    
+                                                          'class': classes})  
+                  
+            
     
     class Meta:
         model = AppCredentials
